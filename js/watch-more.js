bab3c0bb2ab12a46ecd1984d5ca2bb82c6b473f1
shukina7olga <olga.shchukina.99@mail.ru>
2023-06-22 20:29:56 +0300

    portfoloio

---
+++ js/watch-more.js
@@ -0,0 +1,17 @@
+// let moreBtn = document.getElementById('more');
+// let projects = document.querySelectorAll('.portfolio__item');
+
+// for (let i = 15; i < projects.length; i++) {
+//     projects[i].style.display = "none";
+// }
+
+// let count = 6;
+
+// moreBtn.addEventListener('click', e => {
+//     let projects = document.querySelectorAll('.portfolio__item');
+//     if (count <= projects.length){
+//         for(let i=0; i<count ;i++){
+//             projects[i].style.display = "block";
+//         }
+//     }
+// })
